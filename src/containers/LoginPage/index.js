8b46ed935d8d9c59017e8b87890e852efe4ee66e
Ratul Shariar <53607825+straightintocoding@users.noreply.github.com>
2020-08-14 13:32:06 +0600

    signup

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import Layout from '../../components/Layout';
-import Card from '../../components/UI/Card';
-import { signin, isLoggedInUser } from '../../actions';
-import './style.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Layout";
+
+import { signin, isLoggedInUser } from "../../actions";
+import "./style.css";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 /**
-* @author
-* @function LoginPage
-**/
+ * @author
+ * @function LoginPage
+ **/
 
 const LoginPage = (props) => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const dispatch = useDispatch();
-  const auth = useSelector(state => state.auth);
+  const auth = useSelector((state) => state.auth);
 
   // useEffect(() => {
   //   if(!auth.authenticated){
@@ -24,69 +23,68 @@ const LoginPage = (props) => {
   //   }
   // }, []);
 
-
-
-
   const userLogin = (e) => {
     e.preventDefault();
 
-    if(email == ""){
+    if (email == "") {
       alert("Email is required");
       return;
     }
-    if(password == ""){
+    if (password == "") {
       alert("Password is required");
       return;
     }
 
     dispatch(signin({ email, password }));
-    
+  };
 
-
-
-
+  if (auth.authenticated) {
+    return <Redirect to={`/`} />;
   }
 
-
-  if(auth.authenticated){
-    return <Redirect to={`/`} />
-  }
-
-
-
-  return(
+  return (
     <Layout>
-      <div className="loginContainer">
-        <Card>
-          <form onSubmit={userLogin}>
-            
-            <input 
+      <div className="body">
+        <form onSubmit={userLogin} className="card">
+          <h5 className="hd">Login</h5>
+          <p className="title">Enter your details below to continue </p>
+
+          <div className="field">
+            <i class="fa fa-envelope"></i>
+
+            <input
+              required
               name="email"
+              className="in"
               type="text"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Email"
             />
+            <br />
+          </div>
+          <br />
 
-            <input 
+          <div className="field">
+            <i class="fa fa-lock"></i>
+
+            <input
+              required
+              className="in"
               name="password"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
             />
+          </div>
 
-
-            <div>
-              <button>Login</button>
-            </div>
-
-          </form>
-        </Card>
+          <br />
+          <button className="login">Login </button>
+        </form>
       </div>
     </Layout>
-   )
+  );
+};
 
- }
-
-export default LoginPage
+export default LoginPage;
